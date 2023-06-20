5dbd171419e42ee8f9a9bd3d26587b9fb25dc591
Mohamed Attahri <mohamed@posterity.life>
2023-06-20 10:40:46 -0400

    Fixed writing to a closed writer.

--- writer.go
+++ writer.go
@@ -2,6 +2,7 @@ package pgfs
 
 import (
 	"hash"
+	"io/fs"
 
 	"github.com/google/uuid"
 )
@@ -14,22 +15,31 @@ type writer struct {
 	oid         OID
 	id          uuid.UUID
 	contentType string
-	written     int64
+	size        int64
 	hasher      hash.Hash
 	fsys        *FS
+	closed      bool
 }
 
+// Write implements [io.WriteCloser].
 func (w *writer) Write(b []byte) (n int, err error) {
-	n, err = write(w.fsys.conn, w.fd, b)
-	if err != nil {
+	if w.closed {
+		err = fs.ErrClosed
 		return
 	}
-	w.written += int64(n)
+
+	n, err = write(w.fsys.conn, w.fd, b)
+	w.size += int64(n)
 	w.hasher.Write(b[:n])
 	return
 }
 
+// Close implements [io.WriteCloser].
 func (w *writer) Close() error {
+	if w.closed {
+		return fs.ErrClosed
+	}
+
 	const q = `
 	  INSERT INTO pgfs_metadata (
 			oid, id, 
@@ -40,9 +50,13 @@ func (w *writer) Close() error {
 			$3, $4, $5
 		)
   `
-	_, err := w.fsys.conn.Exec(q, w.oid, w.id, w.written, w.contentType, w.hasher.Sum(nil))
-	if err != nil {
+	if _, err := w.fsys.conn.Exec(q, w.oid, w.id, w.size, w.contentType, w.hasher.Sum(nil)); err != nil {
 		return err
 	}
-	return close(w.fsys.conn, w.fd)
+	if err := close(w.fsys.conn, w.fd); err != nil {
+		return err
+	}
+
+	w.closed = true
+	return nil
 }
